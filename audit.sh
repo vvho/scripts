bd8bc76a208f86ee5745b2cd8e62c818058b5c15
vvho <unix2linuxer@gmail.com>
2015-07-24 15:29:03 +0800

    Modify LXX to _EOF_

--- audit.sh
+++ audit.sh
@@ -29,7 +29,7 @@ rm -f /etc/profile.d/audit.sh
 fi
 
 #写入/etc/profile.d/audit.sh
-cat >>/etc/profile.d/audit.sh<<'LXX'
+cat >>/etc/profile.d/audit.sh<<'_EOF_'
 #!/bin/bash
 REAL_USER=$(who -u am i | awk '{print $1}')
 if [ ! -d "/var/log/.audit/$REAL_USER" ];then
@@ -59,5 +59,4 @@ readonly HISTORY_FILE
 export PROMPT_COMMAND='{ USER=$(who -u am i | awk "{print \$1}");PTS=$(who -u am i | awk "{print \$2}");LOGIN_TIME=$(who -u am i|awk "{print \$3,\$4}");PID=$(who -u am i | awk "{print \$(NF-1)}");IP=$(who -u am i | awk "{print \$NF}");EUSER=$(whoami);CURRENT_COMMAND=$(history 2|awk "NR==2 {\$1=\"\";\$2=\"\";\$3=\"\";print \$0}");COMMAND_ID=$(history 2|awk "NR==2 {print \$1}");COMMAND_TIME=$(history 2 | awk "NR==2 {print \$3}");LAST_COMMAND=$(history 2|awk "NR==1 {\$1=\"\";\$2=\"\";\$3=\"\";print \$0}");LAST_COMMAND_ID=$(history 2|awk "NR==1 {print \$1}");COMMAND_TIME=$(history 2 | awk "NR==2 {print \$3}");if [ "${COMMAND_ID}x" != "${LAST_COMMAND_ID}x" ];then echo "$USER      $PTS        $LOGIN_TIME  $IP   EUSE:$EUSER   $COMMAND_ID    [$CURRENT_COMMAND  ]    $COMMAND_TIME";fi; } >>$HISTORY_FILE' &>/dev/null
 
 readonly PROMPT_COMMAND
-LXX
-
+_EOF_
